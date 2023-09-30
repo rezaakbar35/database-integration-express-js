43a264ad9754cc0b3b191f415ee9c41b574384f7
rezaakbar35 <rezaakbarattariq@gmail.com>
2023-09-30 23:41:53 +0700

    Add seeding database feature

---
+++ db/seeding.sql
@@ -0,0 +1,6 @@
+INSERT INTO actor (actor_id, first_name, last_name, last_update) VALUES
+(201, 'Prilly', 'Latuconsina', '2023-09-29 08:22:30'),
+(202, 'Reza', 'Rahadian', '2023-09-29 08:22:30'),
+(203, 'Tara', 'Basro', '2023-09-29 08:22:30'),
+(204, 'Angga', 'Yunanda', '2023-09-29 08:22:30'),
+(205, 'Ari', 'Irham', '2023-09-29 08:22:30');
